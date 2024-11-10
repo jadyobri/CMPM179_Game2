21fd03163b9cf596704ff020656a993cdddcc1f6
Jack O'Brien <jadyobri@ucsc.edu>
2024-11-09 17:26:59 -0800

    GameManager added

---
+++ CMPM179_Game2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                Debug.LogError("Game Manager is NULL");
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
